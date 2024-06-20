2ada49aaf4e5ab18694c7d2e163606b9beea7392
KaterinaSv14 <ekaterinastecenko898@gmail.com>
2024-06-20 23:15:05 +0300

    Homework_22

---
+++ Homework_22.py
@@ -0,0 +1,42 @@
+
+from pydantic.v1 import BaseModel, Field, validator, root_validator, EmailStr
+from typing import List
+
+
+class Product(BaseModel):
+    id: str = Field(regex='^[A-Za-z0-9-]*$')
+    name: str
+    category: str
+    price: int
+    quantity_in_stock: int
+
+    @validator('price', 'quantity_in_stock')
+    def results(cls, value: int) -> int:
+        if value < 50:
+            raise ValueError('Error')
+        return value
+
+
+class OrderItem(BaseModel):
+    product: Product
+    quantity: int
+
+
+class Order(BaseModel):
+    id: str = Field(regex='^[A-Za-z0-9-]*$')
+    order_date: str
+    customer_id: str
+    items: List[OrderItem]
+    status: str
+
+
+class Customer(BaseModel):
+    id: str = Field(regex='^[A-Za-z0-9-]*$')
+    name: str
+    email: EmailStr
+
+    @validator('email')
+    def email(cls, value):
+        if not value.endswith('@example.com'):
+            raise ValueError('Only example.com emails are allowed')
+        return value
